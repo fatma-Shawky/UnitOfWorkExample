b97718fe514d26ec3a14304db57556fcf0c234be
Fatma Shawky <eng.fatma.shawky1@gmail.com>
2024-07-23 20:28:12 +0300

    x

---
+++ UnitOfWorkExample/Controllers/CategoryController.cs
@@ -0,0 +1,79 @@
+﻿using AutoMapper;
+using Microsoft.AspNetCore.Mvc;
+using UnitOfWorkExample.Core.DTOs;
+using UnitOfWorkExample.core.Entities;
+using UnitOfWorkExample.Infrastructure.UnitOfWork;
+
+namespace MyProject.API.Controllers
+{
+    [ApiController]
+    [Route("api/[controller]")]
+    public class CategoriesController : ControllerBase
+    {
+        private readonly IUnitOfWork _unitOfWork;
+        private readonly IMapper _mapper;
+
+        public CategoriesController(IUnitOfWork unitOfWork, IMapper mapper)
+        {
+            _unitOfWork = unitOfWork;
+            _mapper = mapper;
+        }
+
+        [HttpGet]
+        public async Task<IActionResult> GetAll()
+        {
+            var categories = await _unitOfWork.Categories.GetAllAsync();
+            var categoryDtos = _mapper.Map<IEnumerable<CategoryDto>>(categories);
+            return Ok(categoryDtos);
+        }
+
+        [HttpGet("{id}")]
+        public async Task<IActionResult> GetById(int id)
+        {
+            var category = await _unitOfWork.Categories.GetByIdAsync(id);
+            if (category == null)
+            {
+                return NotFound();
+            }
+            var categoryDto = _mapper.Map<CategoryDto>(category);
+            return Ok(categoryDto);
+        }
+
+        [HttpPost]
+        public async Task<IActionResult> Create(CategoryDto categoryDto)
+        {
+            var category = _mapper.Map<Category>(categoryDto);
+            await _unitOfWork.Categories.AddAsync(category);
+            await _unitOfWork.CompleteAsync();
+            return CreatedAtAction(nameof(GetById), new { id = category.Id }, categoryDto);
+        }
+
+        [HttpPut("{id}")]
+        public async Task<IActionResult> Update(int id, CategoryDto categoryDto)
+        {
+            if (id != categoryDto.Id)
+            {
+                return BadRequest();
+            }
+
+            var category = _mapper.Map<Category>(categoryDto);
+            _unitOfWork.Categories.Update(category);
+            await _unitOfWork.CompleteAsync();
+            return NoContent();
+        }
+
+        [HttpDelete("{id}")]
+        public async Task<IActionResult> Delete(int id)
+        {
+            var category = await _unitOfWork.Categories.GetByIdAsync(id);
+            if (category == null)
+            {
+                return NotFound();
+            }
+
+            _unitOfWork.Categories.Delete(category);
+            await _unitOfWork.CompleteAsync();
+            return NoContent();
+        }
+    }
+}
---
+++ UnitOfWorkExample/Controllers/ProductsController.cs
@@ -0,0 +1,80 @@
+﻿using AutoMapper;
+using UnitOfWorkExample.Core.DTOs;
+using UnitOfWorkExample.core.Entities;
+using UnitOfWorkExample.Infrastructure.UnitOfWork;
+using Microsoft.AspNetCore.Mvc;
+
+namespace MyProject.API.Controllers
+{
+    [ApiController]
+    [Route("api/[controller]")]
+    public class ProductsController : ControllerBase
+    {
+        private readonly IUnitOfWork _unitOfWork;
+        private readonly IMapper _mapper;
+
+        public ProductsController(IUnitOfWork unitOfWork, IMapper mapper)
+        {
+            _unitOfWork = unitOfWork;
+            _mapper = mapper;
+        }
+
+        [HttpGet]
+        public async Task<IActionResult> GetAll()
+        {
+            var products = await _unitOfWork.Products.GetAllAsync();
+            var productDtos = _mapper.Map<IEnumerable<ProductDto>>(products);
+            return Ok(productDtos);
+        }
+
+        [HttpGet("{id}")]
+        public async Task<IActionResult> GetById(int id)
+        {
+            var product = await _unitOfWork.Products.GetByIdAsync(id);
+            if (product == null)
+            {
+                return NotFound();
+            }
+            var productDto = _mapper.Map<ProductDto>(product);
+            return Ok(productDto);
+        }
+
+        [HttpPost]
+        public async Task<IActionResult> Create(ProductDto productDto)
+        {
+            var product = _mapper.Map<Product>(productDto);
+            await _unitOfWork.Products.AddAsync(product);
+            await _unitOfWork.CompleteAsync();
+            return CreatedAtAction(nameof(GetById), new { id = product.Id }, productDto);
+        }
+
+        [HttpPut("{id}")]
+        public async Task<IActionResult> Update(int id, ProductDto productDto)
+        {
+            if (id != productDto.Id)
+            {
+                return BadRequest();
+            }
+
+            var product = _mapper.Map<Product>(productDto);
+            _unitOfWork.Products.Update(product);
+            await _unitOfWork.CompleteAsync();
+            return NoContent();
+        }
+
+        [HttpDelete("{id}")]
+        public async Task<IActionResult> Delete(int id)
+        {
+            var product = await _unitOfWork.Products.GetByIdAsync(id);
+            if (product == null)
+            {
+                return NotFound();
+            }
+
+            _unitOfWork.Products.Delete(product);
+            await _unitOfWork.CompleteAsync();
+            return NoContent();
+        }
+    }
+}
+
